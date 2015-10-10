ae6e26ca35fbd3d5af8338e1dd1a6f7ac72bd385
Pedro Luis <luistyle03@gmail.com>
2015-10-09 20:10:16 -0500

    se actualizo clase_1

--- src/projecto_eclipse/clase_1.java
+++ src/projecto_eclipse/clase_1.java
@@ -3,7 +3,7 @@ package projecto_eclipse;
 public class clase_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub dddd
 
 	}
 
